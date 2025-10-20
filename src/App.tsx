bad45d1f12106a65463a60cd96d42ac5a6b7916f
Hamza Simsek <hamzas4011@hotmail.com>
2025-10-20 22:06:45 +0200

    taskinput

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import TaskInput from './components/TaskInput';
+
 export default function App() {
-  return <h1 className="text-2xl font-bold text-center mt-8">My To-Do App</h1>;
+    return (
+  <div> 
+     <h1 className="text-2xl font-bold text-center mt-8">My To-Do App</h1>
+      <TaskInput />
+  </div>
+    );
 }
