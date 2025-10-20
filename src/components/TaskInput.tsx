a7102bd5b05bdaf980437991f9937e0c98829f31
Hamza Simsek <hamzas4011@hotmail.com>
2025-10-20 22:20:45 +0200

    added usestate

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 export default function TaskInput() {
+    const [task, setText] = useState('');
     return (
         <div className="task-input">
             <input type="text" placeholder="Enter your task here..." />
